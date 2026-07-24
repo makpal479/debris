462392cc9900c7960b4a3439978217abf4c09f62
Makpal <makpalkalelova3@gmail.com>
2026-07-25 02:56:54 +0500

    Configure SFML and create game window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    std::cout << "Hello PixelSandbox!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "PixelSandbox");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color(30, 30, 30));
+        window.display();
+    }
+
     return 0;
 }
